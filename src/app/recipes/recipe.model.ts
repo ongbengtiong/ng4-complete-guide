d5cd0689ba5d89f622ac518df1443fbae4a164d6
Ong Beng Tiong <ongbt@i2r.a-star.edu.sg>
2018-04-21 14:26:21 +0800

    Lesson 79

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -4,8 +4,8 @@ export class Recipe {
     public imagePath: string;
 
     constructor(name: string, description: string, imagePath: string) {
-        this.name= name;
+        this.name = name;
         this.description = description;
-        this.imagePath =imagePath;
+        this.imagePath = imagePath;
     }
 }
