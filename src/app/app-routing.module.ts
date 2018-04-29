c15c39e226ed0285a4c409051add399d4661fca8
Ong Beng Tiong <ongbt@i2r.a-star.edu.sg>
2018-04-29 16:56:43 +0800

    Lesson 150

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    { path: 'recipes', component: RecipesComponent, children: [
-        {path: '', component: RecipeStartComponent},
-        {path: ':id', component: RecipeDetailComponent}
-    ] },
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    {
+        path: 'recipes', component: RecipesComponent, children: [
+            { path: '', component: RecipeStartComponent },
+            { path: ':id', component: RecipeDetailComponent }
+        ]
+    },
     { path: 'shopping-list', component: ShoppingListComponent }
 ]
 @NgModule({
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -27,4 +27,8 @@ export class RecipeService {
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
   }
+
+  getRecipe(id: number) {
+    return this.recipes[id];
+  }
 }
