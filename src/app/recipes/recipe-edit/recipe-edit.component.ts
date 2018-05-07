a867374adaebea760835819ef54df2cd723a0934
Ong Beng Tiong <ongbt@i2r.a-star.edu.sg>
2018-05-07 18:27:43 +0800

    Lesson 217

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -55,4 +55,14 @@ export class RecipeEditComponent implements OnInit {
   onSubmit() {
     console.log(this.recipeForm);
   }
+
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    );
+  }
 }
+
