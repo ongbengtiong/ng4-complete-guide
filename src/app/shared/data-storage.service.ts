b3727cd5e88402d02aadd595da235f56b40a39a2
Ong Beng Tiong <ongbt@i2r.a-star.edu.sg>
2018-05-27 20:59:43 +0800

    Lesson 252

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,22 +2,34 @@ import { Http, Response } from "@angular/http";
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import 'rxjs/Rx';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService ) {
+    constructor(private http: Http, private recipeService: RecipeService) {
 
     }
     storeRecipes() {
         return this.http.put('https://ng-recipe-book-1fb60.firebaseio.com/recipes.json', this.recipeService.getRecipes());
     }
     getRecipes() {
-        return this.http.get('https://ng-recipe-book-1fb60.firebaseio.com/recipes.json')
-            .subscribe(
+        this.http.get('https://ng-recipe-book-1fb60.firebaseio.com/recipes.json')
+            .map(
                 (response: Response) => {
                     const recipes: Recipe[] = response.json();
+                    for (let recipe of recipes) {
+                        if (!recipe['ingredients']) {
+                            console.log(recipe);
+                            recipe['ingredients'] = [];
+                        }
+                    }
+                    return recipes;
+                }
+            )
+            .subscribe(
+                (recipes: Recipe[]) => {
                     this.recipeService.setRecipes(recipes);
-                }    
-        );
+                }
+            );
     }
 } 
