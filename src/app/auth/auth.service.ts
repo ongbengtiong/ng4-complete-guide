76fbe98742b47d8430cd0f4c4554ebc0b1b8fc99
Ong Beng Tiong <ongbt@i2r.a-star.edu.sg>
2018-06-02 14:27:33 +0800

    Lesson 259

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,7 +7,17 @@ export class AuthService {
   constructor() { }
 
   signupUser(email: string, password: string) {
-    firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(email, password)
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(
+        error => console.log(error)
+      )
+  }
+
+  signinUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword (email, password)
+      .then(
+        response => console.log(response)
+      )
       .catch(
         error => console.log(error)
       )
