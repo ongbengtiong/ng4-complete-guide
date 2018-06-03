6fd9bcf89a4bef7f42fa456378a0be226ac989b3
Ong Beng Tiong <ongbt@i2r.a-star.edu.sg>
2018-06-03 15:10:02 +0800

    Lesson 264

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthService {
   token: string;
-  constructor() { }
+  constructor(private router: Router) { }
 
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -18,9 +19,11 @@ export class AuthService {
       .then(
         response => {
           console.log(response);
+          this.router.navigate(['/']);
           firebase.auth().currentUser.getIdToken().then(
             (token: string) => {
               this.token = token;
+
             }
           );
         })
