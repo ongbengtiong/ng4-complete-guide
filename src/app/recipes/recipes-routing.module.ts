794f43245a49401e2e372eb18c52f205800a7f54
Ong Beng Tiong <ongbt@i2r.a-star.edu.sg>
2018-06-16 16:31:33 +0800

    Lesson 285

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -23,7 +23,10 @@ const recipesRoutes: Routes = [
     imports: [
         RouterModule.forChild(recipesRoutes)
     ],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers: [
+        AuthGuard
+    ]
 })
 export class RecipesRoutingModule {
 
