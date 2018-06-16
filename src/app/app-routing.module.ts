4bf113986a6f0174871ef2688714e0c98eed6235
Ong Beng Tiong <ongbt@i2r.a-star.edu.sg>
2018-06-16 21:08:32 +0800

    Lesson 288

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, Component } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
 import { Recipe } from "./recipes/recipe.model";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { HomeComponent } from "./core/home/home.component";
@@ -10,7 +10,7 @@ const appRoutes: Routes = [
 
 ];
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
+    imports: [RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules })],
     exports: [RouterModule]
 })
 export class AppRoutingModule {
